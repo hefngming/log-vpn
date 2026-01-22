36afbb32f90926df7787625d0dafed4adbf2a2ce
hefngming <siumingfilm@hotmail.com>
2026-01-22 15:40:40 +0800

    Update main.ts

--- client-src/main.ts
+++ client-src/main.ts
@@ -17,8 +17,7 @@ function createWindow() {
   });
 
   if (app.isPackaged) {
-    // 关键：main.js 在 dist_electron，index.html 在 dist，它们是平级目录
-    // 路径：先退出 dist_electron，再进入 dist
+    // 关键：从 dist_electron 向上跳一级找到并行的 dist 文件夹
     win.loadFile(path.join(__dirname, '../dist/index.html'));
   } else {
     win.loadURL('http://localhost:5173');
